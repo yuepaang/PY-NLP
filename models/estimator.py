5e5f030310e934eb2d4051392997de4064a22ab0
Yue Peng <ypeng7@outlook.com>
2018-10-13 23:42:53 +0800

    parsearg

---
+++ models/estimator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Dataset, Estimator API of Tensorflow
+
+# @Date    : 2018-10-13
+# @Author  : Yue Peng (ypeng7@outlook.com)
+"""
+import os
+import sys
+import tensorflow as tf
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
--- utils/segmenter.py
+++ utils/segmenter.py
@@ -6,7 +6,9 @@ AUTHOR: Yue Peng
 EMAIL: ypeng7@outlook.com
 DATE: 2018.10.02
 """
-import os, sys, codecs, re, pickle
+import os, sys
+import codecs, re, pickle
+import argparse
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 import jieba
 import jieba.posseg as pseg
@@ -19,7 +21,6 @@ from nltk.stem.porter import PorterStemmer
 import langid
 import logging
 jieba.setLogLevel(logging.INFO)
-
 from config import Config
 
 
@@ -167,13 +168,17 @@ class Segmenter(object):
         return res
 
 
-def main():
+def main(args):
     cut = Segmenter()
-    print(cut.process_sentence('往饭卡里充的钱没有充进去怎么办？'))
-    print(cut.process_sentence('I have a pen.'))
-    print(cut.process_sentence('I have 1张饭卡.'))
-    print(cut.extract_noun('往饭卡里充的钱没有充进去怎么办？'))
+    print(cut.process_sentence(args.sentence))
+    # print(cut.process_sentence('往饭卡里充的钱没有充进去怎么办？'))
+    # print(cut.process_sentence('I have a pen.'))
+    # print(cut.process_sentence('I have 1张饭卡.'))
+    # print(cut.extract_noun('往饭卡里充的钱没有充进去怎么办？'))
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Enter an sentence!")
+    parser.add_argument("--sentence", help="input an sentence you want to segment please~~", type=str)
+    args = parser.parse_args()
+    main(args)
